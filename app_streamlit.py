665c57b33f40462827e1200b1e95137d6d4a896a
NTAMBARA Etienne <65026243+94etienne@users.noreply.github.com>
2025-08-31 20:42:21 +0300

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -926,7 +926,7 @@ def main():
                         file_name=f"rp_student_data_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv",
                         mime="text/csv",
                         use_container_width=True,
-                        disabled=True
+                        disabled=False
                     )
                 else:
                     st.button("Download CSV", disabled=True, use_container_width=True)
